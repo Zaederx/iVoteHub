35844c7886d1af054e7ed1969d248d1768caa1b5
ZIshmael <zacharyk.ishmael@gmail.com>
2022-07-12 15:41:02 +0200

    WebConfig.java - removed recent changes - potential cause of bugs

--- src/main/java/app/iVoteHub/config/WebConfig.java
+++ src/main/java/app/iVoteHub/config/WebConfig.java
@@ -4,27 +4,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 //import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer{
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-		WebMvcConfigurer.super.addResourceHandlers(registry);
+		// WebMvcConfigurer.super.addResourceHandlers(registry);
 	}
 	
 	/*Allows bootstrap to be used with Spring security*/
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable("iVoteHub");
-		WebMvcConfigurer.super.configureDefaultServletHandling(configurer);
+		// WebMvcConfigurer.super.configureDefaultServletHandling(configurer);
 	}
 	 
 	@Bean
